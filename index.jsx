46d2d36d86c8c6fc14c8164e4649588444b092cc
mLondono <mlondono@q10.com>
2022-09-08 14:51:02 -0500

    chore: init proyect

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+ReactDom.createRoot(document.querySelector('#root')).render(
+    <React.StrictMode>
+        <h1>Hola mundo</h1>
+    </React.StrictMode>
+)
